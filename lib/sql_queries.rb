c82e132244375acefd40cffaaecf6f6cc3875da7
Brandon Leeds <brandonleeds@gmail.com>
2018-01-14 21:02:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,6 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
+GROUP BY projects.title
 WHERE SUM(pledges.amount) >= projects.funding_goal;"
 end
 
